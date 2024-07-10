70e6f3b102c9d519e5b3261c5db2e70566ae4d5d
pratikshajadhav1021 <pratiksha10301@gmail.com>
2024-07-10 20:36:58 +0530

    Add files via upload

---
+++ admin/home.php
@@ -0,0 +1,88 @@
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+RECRUITMENT DASHBOARD
+      
+      </h1>
+     
+    </section>
+
+ 
+    <section class="content">
+ 
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>300</h3>
+
+              <p>Visitors</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+          </div>
+        </div>
+        
+
+        <div class="col-lg-3 col-xs-6">
+   
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>230</h3>
+
+              <p>Registered Users</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>30</h3> 
+              <p>Registered Companies</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      
+       
+          <div class="box box-info">
+            <div class="box-header">
+              <i class="fa fa-envelope"></i>
+
+              <h3 class="box-title">Quick Email</h3>
+           
+            </div>
+            <div class="box-body">
+              <form action="#" method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" name="emailto" placeholder="Email to:">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" placeholder="Subject">
+                </div>
+                <div>
+                  <textarea class="textarea" placeholder="Message" style="width: 100%; height: 125px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                </div>
+              </form>
+           
+         
+              <button type="button" class="pull-right btn btn-default" id="sendEmail">Send</button>
+           </div>
+          </div>
+
+  
+    </section>
+   
+  
